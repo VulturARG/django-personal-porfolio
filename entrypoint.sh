5b6715455ee70d301a096a9727dd14f615624264
VulturARG <luisbriones@gmail.com>
2021-01-07 14:06:27 -0300

    First attempt

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@
 python3 manage-prod.py migrate
 
 # python3 manage-prod.py collectstatic --no-input
-python3 manage-prod.py loaddata ./portfolio/pre_data.json
+python3 manage-prod.py loaddata pre_data.json
+
+## docker-compose exec web python manage.py createsuperuser
+## docker-compose exec django_gunicorn bash 
 
 gunicorn portfolio.wsgi:application --bind 0.0.0.0:8000
 
