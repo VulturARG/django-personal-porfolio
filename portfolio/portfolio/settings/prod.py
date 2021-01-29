6dc244b94d23ad4b184b3d50d75f631cc8c25a88
luis <lbriones@arnet.com.ar>
2021-01-29 18:05:51 -0300

    sqlite in prod

--- portfolio/portfolio/settings/prod.py
+++ portfolio/portfolio/settings/prod.py
@@ -23,13 +23,20 @@ INSTALLED_APPS = [
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db_postgres',
+#         'PORT': 5432
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db_postgres',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
