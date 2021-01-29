bb0833c20696289afec5fadc16bedd8dfb9f3f85
luis <lbriones@arnet.com.ar>
2021-01-29 18:50:19 -0300

    Debug False

--- portfolio/portfolio/settings/prod.py
+++ portfolio/portfolio/settings/prod.py
@@ -1,9 +1,8 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 # load production server from .env
-#ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',os.getenv('SERVER', default='127.0.0.1'),'django']
 #ALLOWED_HOSTS = ['*']
 
