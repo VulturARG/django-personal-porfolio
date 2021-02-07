fb31875fe778c570bdbb72c8a184f8f134ff3059
VulturARG <luisbriones@gmail.com>
2021-02-07 16:45:54 -0300

    Delete directory static, migrate y collecstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# python3 manage-prod.py migrate --no-input
+rm -rf static
 
-# python3 manage-prod.py collectstatic --no-input
+python3 manage-prod.py migrate --no-input
+
+python3 manage-prod.py collectstatic --no-input
 
 gunicorn portfolio.wsgi:application --bind 0.0.0.0:8000
 
