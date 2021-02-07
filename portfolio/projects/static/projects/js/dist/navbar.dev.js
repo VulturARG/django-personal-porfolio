3a8989e5863623d5ca4853c274623dc0e4005106
VulturARG <luisbriones@gmail.com>
2021-02-07 17:28:36 -0300

    Remplace bar button by times button

--- portfolio/projects/static/projects/js/dist/navbar.dev.js
+++ portfolio/projects/static/projects/js/dist/navbar.dev.js
@@ -4,6 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
   var button = document.getElementById("menu-button");
   button.addEventListener("click", function () {
     var sidebar = document.getElementById("sidebar");
-    if (sidebar.classList.contains('show-sidebar')) sidebar.classList = "sidebar";else sidebar.classList = "sidebar show-sidebar";
+    var icon = document.getElementById('fas');
+
+    if (sidebar.classList.contains('show-sidebar')) {
+      sidebar.classList = "sidebar";
+      icon.classList.replace('fa-times', 'fa-bars');
+    } else {
+      sidebar.classList = "sidebar show-sidebar";
+      icon.classList.replace('fa-bars', 'fa-times');
+    }
   });
 });
--- portfolio/projects/static/projects/js/navbar.js
+++ portfolio/projects/static/projects/js/navbar.js
@@ -3,10 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     button.addEventListener("click", function() {
         let sidebar = document.getElementById("sidebar");
+        let icon = document.getElementById('fas');
         
-        if(sidebar.classList.contains('show-sidebar'))
+        if(sidebar.classList.contains('show-sidebar')){
             sidebar.classList = "sidebar";
-        else
+            icon.classList.replace('fa-times','fa-bars');
+        } else {
             sidebar.classList = "sidebar show-sidebar";
+            icon.classList.replace('fa-bars', 'fa-times');
+        }
     });
 });
